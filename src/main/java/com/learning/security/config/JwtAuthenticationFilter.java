887d97e6f4aad0d3ac3f69784e0bf3fc11bc5ea4
Minh Quan <minhquancn1809@gmail.com>
2023-03-09 23:30:14 +0700

    090323 init security java project

---
+++ src/main/java/com/learning/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.learning.security.config;
+
+import com.learning.security.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authentication");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7);
+
+        // Extract email from JWT Token
+        final String userEmail = jwtService.extractUsername(jwt);
+        if(userEmail != null){
+
+        }
+    }
+}
