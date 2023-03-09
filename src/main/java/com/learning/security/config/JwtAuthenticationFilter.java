ff6ecce81c8423fdc36385b2a7bd7664be1b8969
Minh Quan <minhquancn1809@gmail.com>
2023-03-09 23:35:34 +0700

    090323 update userDetailsService in JwtAuthenticationFilter

--- src/main/java/com/learning/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/learning/security/config/JwtAuthenticationFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +20,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -34,8 +38,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         // Extract email from JWT Token
         final String userEmail = jwtService.extractUsername(jwt);
-        if(userEmail != null){
-
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
         }
     }
 }
